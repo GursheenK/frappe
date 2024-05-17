9d62b4c6495455f91780684a62654b9a75ba8605
Gursheen Anand <gursheen@frappe.io>
2024-05-17 18:26:16 +0530

    feat: show list from stored config

--- desk/src/App.vue
+++ desk/src/App.vue
@@ -1,8 +1,8 @@
 <template>
-  <div class="flex w-screen">
-    <div class="border-r border-gray-50 w-[3rem] h-screen bg-gray-50"></div>
-    <div class="flex-col">
-      <div class="border-b border-gray-50 h-[2.5rem]"></div>
+  <div class="flex w-full">
+    <div class="border-r border-gray-200 w-[3rem] h-screen bg-gray-50"></div>
+    <div class="flex-col w-[1400px]">
+      <div class="border-b border-gray-200 h-[2.5rem]"></div>
       <router-view></router-view>
     </div>
   </div>
